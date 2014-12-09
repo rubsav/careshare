36461caafdedf4fc66c1aad4f8ab4cafbc3a3a5e
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-09 17:20:21 -0500

    Updated user model to work with password, updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,46 @@
 class UsersController < ApplicationController
-  def index
-  end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def new
+  	@user = User.new
   end
 
+  def create
+  	@user = User.new(user_params)
+	  	if @user.save
+	  		self.current_user = @user
+	  		redirect_to root_path, notice: "Signed up!"
+	  	else
+	  		render 'new'
+	  end
+	end
+
   def edit
+  	@user = User.find(params[:id])
   end
+
+  def update
+  	@user = User.find(params[:id])
+  		if @user.update_attributes(user_params)
+  			redirect_to @user
+  		else
+  			render :edit
+		end
+  end
+
+  	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to root_path, notice: "User account deleted!"
+	end
+
+
+  private
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   resources :trans_cares
   resources :reviews
-  resources :users
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
