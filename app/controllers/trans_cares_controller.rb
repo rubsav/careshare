51868888b6cbc2a2e3d269cb7356879b989ccbc9
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-12-10 12:51:12 -0500

    Added trans care controller with show new and create methods and an instance variable of provider

--- app/controllers/trans_cares_controller.rb
+++ app/controllers/trans_cares_controller.rb
@@ -1,11 +1,20 @@
 class TransCaresController < ApplicationController
-  def index
-  end
 
   def show
+    @provider = TransCare.all
   end
-
+  
   def new
+    @provider = TransCare.new
+  end
+  
+  def create
+    @provider = Trans_care.new(trans_care_params)
+      if @provider.save
+        redirect_to @provider, notice: "D.O.C Successfully Added!"
+      else
+        render 'new'
+    end
   end
 
   def edit
@@ -13,4 +22,10 @@ class TransCaresController < ApplicationController
 
   def delete
   end
+
+private
+  def trans_care_params
+    params.require(:trans_care).permit(:first_name, :last_name, :address, :phone_number)
+  end
+
 end
