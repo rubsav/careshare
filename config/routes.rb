f724fa35cfa874d3dc1f7eafec42735a62f66ea6
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-09 20:43:12 -0500

    Created log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   
   resources :trans_cares
   resources :reviews
-  resources :users, only: [:new, :create]
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
