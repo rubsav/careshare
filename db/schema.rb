4fdc1c28b6f0e8a2dda21416a84e9a5872f5443f
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-11 16:03:57 -0500

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141211190006) do
 
   create_table "providers", force: true do |t|
