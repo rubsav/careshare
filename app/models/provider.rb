5fe1e7db999013a8cb16fa59a190a700add2c72f
daynaclowe <daynalowe@Dayna-Lowes-MacBook-2.local>
2014-12-12 13:57:29 -0500

    Removed Doctors Counsellors and Organizations from INSIDE the Provider class. Added self method?

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,21 +1,26 @@
 class Provider < ActiveRecord::Base
+	validates :first_name, :last_name, :full_address, :address_line1, :city, :province, :area_code, :phone_number, :type, :waiting_period,  presence: true
 
 	has_many :reviews
 	has_many :users, through: :reviews
-	
-	class Doctor < Provider;
-	end
-
-
-	class Counsellor < Provider;
-	end
-
-
-	class Organization < Provider;
-	end
 
 	def full_address
 		full_address = "#{address_line1} #{address_line2} #{city} #{province} #{area_code}"	
 	end
 
+	def self
+      %w(Doctor Counsellor Organization)
+    end
+
+end
+
+class Doctor < Provider
+end
+
+
+class Counsellor < Provider
+end
+
+
+class Organization < Provider
 end
