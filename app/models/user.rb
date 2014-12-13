3f069654dfc80071271a95587b507a95ca35ac09
daynaclowe <daynalowe@Dayna-Lowes-MacBook-2.local>
2014-12-12 20:15:17 -0500

    Added full name method for users crating reviews via the seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :reviews
   	has_many :providers, through: :reviews
+
+  	def full_name
+  		"#{first_name} #{last_name}"
+  	end
 end
