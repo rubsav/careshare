0d7c64429f23c16f5fe7524634a533ba6fd1117d
daynaclowe <daynalowe@Dayna-Lowes-MacBook-2.local>
2014-12-14 09:19:21 -0500

    Removed subclass and broke them into their own files

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -2,13 +2,23 @@
 	validate :first_name, unless: :is_organization?
 	validate :last_name, unless: :is_organization?
 	validates :full_address, :address_line1, :city, :province, :area_code, :phone_number, :type, :waiting_period,  presence: true
+	
 
 	has_many :reviews
 	has_many :users, through: :reviews
-
 	has_many :ratings
 
 	before_save :capitalize_type
+
+	# delegate :doctors, :counsellors, :organizations, to: :providers
+	# scope :doctors, -> { where(type: 'Doctor') } 
+	# scope :counsellors, -> { where(type: 'Counsellor') } 
+	# scope :organizations, -> { where(type: 'Organization') }
+
+	# def self
+ #      %w(Doctor Counsellor Organization)
+ #    end
+
 
 	def full_address
 		full_address = "#{address_line1} #{address_line2} #{city} #{province} #{area_code}"	
@@ -24,13 +34,3 @@
 
 end
 
-class Doctor < Provider
-end
-
-
-class Counsellor < Provider
-end
-
-
-class Organization < Provider
-end
