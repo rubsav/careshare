9f48a7bf2d08cc50c4fe63281ed5cd2e7493d9c3
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-12-16 16:38:37 -0500

    New and create methods for rating, as well as added column names to params

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -5,17 +5,36 @@ class RatingsController < ApplicationController
     @rating = Rating.find(params[:id])
   end
 
-  def create
-    @rating = Rating.new(rating_params)
+  def new
+    @rating = Rating.new
+  end
 
-    if current_user
-      @rating.user_id = current_user.id
-    end
+  def create
+  #   @rating = Rating.new(rating_params)
+
+  #   if current_user
+  #     @rating.user_id = current_user.id
+  #   end
     
-    @rating.provider_id = @provider.id
+  #   @rating.provider_id = @provider.id
     
+  #   if @rating.save
+  #     redirect_to @provider, notice: 'You have successfully added a rating!'
+  #   else
+  #     render 'providers/show'
+  #   end
+  # end
+
+
+   @rating = @provider.ratings.build(rating_params)
+    # if current_user
+    #   @rating.user = current_user
+    #   @rating.name = current_user.first_name
+    #   @rating.email = current_user.email
+    # end
+
     if @rating.save
-      redirect_to @provider, notice: 'You have successfully added a rating!'
+      redirect_to @provider, notice: 'Rating created succesfully'
     else
       render 'providers/show'
     end
@@ -34,11 +53,11 @@ class RatingsController < ApplicationController
   private
 
   def rating_params
-    params.require(:rating).permit(:rating, :provider_id, :user_id, :type)
+    params.require(:rating).permit(:knowledge_rating, :support_rating, :comfort_rating, :accessibility_rating, :recommendation_rating, :provider_id, :user_id, :type)
   end
 
   def load_provider
-    provider = params[:doctor_id] || params[:counsellor_id] || params[:organization_id]
+    provider = params[:doctor_id] || params[:counsellor_id] || params[:organization_id] || params[:provider_id]
     @provider = Provider.find(provider)
   end
 
