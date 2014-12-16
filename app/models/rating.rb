3a4ffb931bd6a5d76e5db4d852332fc6d653a179
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-12-16 15:24:33 -0500

    Added validations for different types of ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,6 @@
 class Rating < ActiveRecord::Base
-
+	validates :knowledge_rating, :support_rating, :comfort_rating, :accessibility_rating, :recommendation_rating,  presence: true
+	
 	belongs_to :user
 	belongs_to :provider
 
