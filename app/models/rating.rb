004f43fd251a01b58babf03189de1971964e2eb0
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-12-17 10:52:30 -0500

    Added method to determine the average rating on a provider

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -4,4 +4,13 @@ class Rating < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :provider
 
+
+	def single_user_rating
+		# Rating.find(rating.user_id == @current_user).sum("knowledge_rating + support_rating + comfort_rating + accessibility_rating + recommendation_rating ")
+	end
+
+	def overall_rating 
+		Rating.sum("knowledge_rating + support_rating + comfort_rating + accessibility_rating + recommendation_rating ") / (25 * Rating.count(:user_id).to_f) * 100
+	end
+
 end
