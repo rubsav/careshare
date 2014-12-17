f3684a137fcc20b260ee2f3603004f968d5fa437
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-17 16:27:09 -0500

    review create method responds to both html and javascript formats. generated a create.js file to handle the ajax requests related to the display of reviews and review errors

--- app/assets/javascripts/providers.js
+++ app/assets/javascripts/providers.js
@@ -1,12 +1,19 @@
 $(document).on('ready page:load', function() {
-  $('#search-form').submit(function(event) {
+
+ $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
 
       $.getScript('/providers?search=' + searchValue);
       
       });
+
+ $('#new_review').on('ajax:beforeSend', function(){
+    $('input[type=submit]').val('Saving....').attr('disabled','disabled');
   });
 
+  $('#new_review').on('ajax:complete', function(){
+    $('input[type=submit]').val('Create Review').removeAttr('disabled');
+  });
 
-
+ });
