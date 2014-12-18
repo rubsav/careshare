df1be664614b5f339dd27b2bff9770676fdabf9f
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-12-17 21:16:47 -0500

    Created a method that checks to see if that provider has been rated, avoiding nil and is used by the providers helper to check if the current user has rated the provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ActiveRecord::Base
 	has_secure_password
 	has_many :reviews
 	has_many :ratings
-  	has_many :providers, through: :reviews
+  	has_many :providers, through: :reviews, through: :ratings
 
   	def full_name
   		"#{first_name} #{last_name}"
   	end
+
+ #  	def single_user_rating
+	# 	self.provider.ratings("knowledge_rating + support_rating + comfort_rating + accessibility_rating + recommendation_rating ")
+	# end
+
+	def rated?(provider)
+		ratings.find_by(provider: provider)
+	end
+
 end
