fd5a8314a845270480be0bf5504ae677d147dcc7
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-19 15:27:12 -0500

    ensure logged in method takes the session full path and stores it into previous_url value before redirecting to a new session path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,12 +19,13 @@ class ApplicationController < ActionController::Base
   def ensure_logged_in
     unless current_user
       flash[:alert] = "Please log in"
+      session[:previous_url] = request.fullpath
       redirect_to new_session_path
     end
   end
 
   def require_user
-  redirect_to root_path unless current_user
+    redirect_to root_path unless current_user
   end
   
 end
