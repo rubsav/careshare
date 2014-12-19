960447b98cf491be713c763640e70c5761f3a027
Ruben Avila <Ruavilaa@hotmail.com>
2014-12-19 15:29:16 -0500

    The sessions create method takes the user session id/path and stores it before deleting it and inserting the previous url path - (original fullpath) or root path - and redirects the user to that path.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:email])
   		if user && user.authenticate(params[:password])
-  			session[:user_id] = user.id
-  			redirect_to providers_url, :notice => "Logged in!"
+        session[:user_id] = user.id
+        default_url = (session.delete(:previous_url) || root_path)
+  			redirect_to default_url, :notice => "Logged in!"
   		else
   			flash.now[:alert] = "Invalid email or password"
       		render "new"
