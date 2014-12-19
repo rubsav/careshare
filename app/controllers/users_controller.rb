6ae8a5a2273e402148193a64814273d249ab60cb
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-12-18 21:21:17 -0500

    Added new and create exception in the before filter, as well as fixed the redirect when a user creates a profile, automatically logging them in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :require_user
+  before_filter :require_user, except: [:new, :create]
   load_and_authorize_resource
 
   def show
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
 	  	if @user.save
-	  		render 'sessions/new', notice: "Signed up!"
+	  		session[:user_id] = @user.id
+        redirect_to user_path(@user), notice: "Signed up!"
 	  	else
 	  		render 'new'
 	  end
